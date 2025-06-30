cc5c8a813d97ce6a6f3c6be42daf24e4dabddc64
Eguo Wang <wangeguo@gmail.com>
2025-06-30 13:50:51 +0800

    refactor: optimze StatusBadge componenet

--- components/stage/stage-status.tsx
+++ components/stage/stage-status.tsx
@@ -2,40 +2,36 @@ import { Badge } from "@/components/ui/badge";
 import { CircleCheck } from "lucide-react";
 import { StageStatus } from "@/types/stage-status";
 
-interface StatusBadgeProps {
+interface StatusProps {
   status: StageStatus;
 }
 
-export function StatusBadge({ status }: StatusBadgeProps) {
-  switch (status) {
-    case StageStatus.Pending:
-      return (
-        <Badge
-          variant="outline"
-          className="bg-gray-100 border-gray-500 text-gray-700 rounded-2xl font-bold"
-        >
-          {status}
-        </Badge>
-      );
-    case StageStatus.InProgress:
-      return (
-        <Badge
-          variant="outline"
-          className="bg-yellow-100 border-yellow-500 text-yellow-700 rounded-2xl font-bold"
-        >
-          {status}
-        </Badge>
-      );
-    case StageStatus.Completed:
-      return (
-        <Badge
-          variant="outline"
-          className="bg-green-100 border-green-500 text-green-700 rounded-2xl font-bold"
-        >
-          <CircleCheck className="mr-1" /> {status}
-        </Badge>
-      );
-    default:
-      return null;
-  }
+export function StatusBadge({ status }: StatusProps) {
+  const statusMap = {
+    [StageStatus.Pending]: {
+      className: "bg-gray-100 border-gray-500 text-gray-700",
+      icon: null,
+    },
+    [StageStatus.InProgress]: {
+      className: "bg-yellow-100 border-yellow-500 text-yellow-700",
+      icon: null,
+    },
+    [StageStatus.Completed]: {
+      className: "bg-green-100 border-green-500 text-green-700",
+      icon: <CircleCheck />,
+    },
+  };
+
+  const config = statusMap[status];
+  if (!config) return null;
+
+  return (
+    <Badge
+      variant="outline"
+      className={`${config.className} rounded-2xl font-bold`}
+    >
+      {config.icon}
+      {status}
+    </Badge>
+  );
 }
