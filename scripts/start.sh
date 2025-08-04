c15db2e7314223bfe82d2082075fcbfd6d8feb9b
Eguo Wang <wangeguo@gmail.com>
2025-08-04 19:56:22 +0800

    fix: allow empty values for NEXT_PUBLIC_ANALYTICS_ID

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-# Define a list of environment variables to check and replace
-VARIABLES=("NEXT_PUBLIC_BACKEND_URL" "NEXT_PUBLIC_ANALYTICS_ID")
+# Define a list of mandatory environment variables to check
+MANDATORY_VARS=("NEXT_PUBLIC_BACKEND_URL")
 
-# Check if each variable is set
-for VAR in "${VARIABLES[@]}"; do
+# Define a list of optional environment variables (no check needed)
+OPTIONAL_VARS=("NEXT_PUBLIC_ANALYTICS_ID")
+
+# Check if each mandatory variable is set
+for VAR in "${MANDATORY_VARS[@]}"; do
     if [ -z "${!VAR}" ]; then
         echo "$VAR is not set. Please set it and rerun the script."
         exit 1
     fi
 done
 
+# Combine mandatory and optional variables for replacement
+ALL_VARS=("${MANDATORY_VARS[@]}" "${OPTIONAL_VARS[@]}")
+
 # Find and replace BAKED values with real values
 find /app/public /app/.next -type f -print0 | xargs -0 grep -i -l "BAKED_" |
 while read -r file; do
-    for VAR in "${VARIABLES[@]}"; do
+    for VAR in "${ALL_VARS[@]}"; do
         sed -i "s|BAKED_$VAR|${!VAR}|g" "$file"
     done
 done
