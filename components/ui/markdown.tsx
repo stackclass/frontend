7ece3149466a3c5b99e61a45a8e6a2dcc3a6274f
Eguo Wang <wangeguo@gmail.com>
2025-09-07 19:34:23 +0800

    fix: 'node' is assigned a value but never used error

--- components/ui/markdown.tsx
+++ components/ui/markdown.tsx
@@ -52,7 +52,7 @@ export default function Markdown(props: MarkdownProps) {
           rehypePlugins={rehypePlugins}
           components={{
             code(props) {
-              const { children, className, node, ...rest } = props;
+              const { children, className, ...rest } = props;
               const match = /language-(\w+)/.exec(className || "");
               const code = String(children).replace(/\n$/, "");
 
