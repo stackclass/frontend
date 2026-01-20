0bfc5e1d8995c3100edf10a0c43c103df2a4c4cf
Eguo Wang <wangeguo@gmail.com>
2026-01-20 17:48:26 +0800

    chore: upgrade Next.js to version 16
    - Bump next from 15.5.9 to 16.1.4
    - Bump @next/third-parties and eslint-config-next to 16.1.4
    - Update Node.js to v22 in Dockerfile and CI workflow
    - Migrate ESLint to flat config format (next lint removed in v16)
    - Remove --turbopack flag from dev script (now default)
    
    Closes #19

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import nextPlugin from "@next/eslint-plugin-next";
+import reactPlugin from "eslint-plugin-react";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    plugins: {
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+    },
+  },
+  {
+    plugins: {
+      react: reactPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs["jsx-runtime"].rules,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    plugins: {
+      "react-hooks": hooksPlugin,
+    },
+    rules: hooksPlugin.configs.recommended.rules,
+  },
+  ...tseslint.configs.recommended,
+  {
+    ignores: [".next/**", "node_modules/**"],
+  },
+];
